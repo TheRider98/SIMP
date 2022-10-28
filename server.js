6bf14e2cabf4448d1ab1ae2cfac67912261885ca
Anthony Perez <aperez214@knights.ucf.edu>
2022-10-28 17:52:54 -0400

    Update package.json

--- server.js
+++ server.js
@@ -170,6 +170,15 @@ app.post("/login", (request, response) => {
     });
 });
 
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
 
 
 // create port
@@ -190,21 +199,11 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+
 // Assign socket object to every request
 app.use(function (req, res, next) {
   req.io = io;
   next();
-});
-
-
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
 });
 
 /*
