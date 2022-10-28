72bf2a7f096ecb4d5d5f23ef4c33dd7df25abe98
Anthony Perez <aperez214@knights.ucf.edu>
2022-10-28 16:52:33 -0400

    Update to package.json

--- server.js
+++ server.js
@@ -198,6 +198,16 @@ app.post('/api/world', (req, res) => {
   );
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'frontend/react-auth/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/react-auth/build', 'index.html'));
+  });
+}
+
+
 /*
 // free endpoint
 app.get("/free-endpoint", (request, response) => {
